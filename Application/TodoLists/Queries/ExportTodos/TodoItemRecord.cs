7bcfaa07a1250734fe1993dde787cbcf3de9c151
Mateus Maia <maiamateusotavio@gmail.com>
2025-06-25 21:35:03 -0300

    Criando Queries GetTodos

---
+++ Application/TodoLists/Queries/ExportTodos/TodoItemRecord.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+
+namespace Application.TodoLists.Queries.ExportTodos;
+
+public class TodoItemRecord : IMapFrom<TodoItem>
+{
+    public string? Title { get; set; }
+
+    public bool Done { get; set; }
+}
