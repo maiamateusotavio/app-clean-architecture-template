3f58d71ef1631fe1496c6fcd182c7d77e96ae538
Mateus Maia <maiamateusotavio@gmail.com>
2025-07-01 22:13:54 -0300

    Criando classes de infra

---
+++ Infrastructure/Files/CsvFileBuilder.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using Application.TodoLists.Queries.ExportTodos;
+using Infrastructure.Files.Maps;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace Infrastructure.Files;
+
+public class CsvFileBuilder : ICsvFileBuilder
+{
+    public byte[] BuildTodoItemsFile(IEnumerable<TodoItemRecord> records)
+    {
+        using var memoryStream = new MemoryStream();
+        using (var streamWriter = new StreamWriter(memoryStream))
+        {
+            using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+            csvWriter.Configuration.RegisterClassMap<TodoItemRecordMap>();
+            csvWriter.WriteRecords(records);
+        }
+
+        return memoryStream.ToArray();
+    }
+}
