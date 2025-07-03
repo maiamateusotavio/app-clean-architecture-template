9ed85fc4f1c04034e6f5647d4ea6ba5f8f52406d
Mateus Maia <maiamateusotavio@gmail.com>
2025-07-02 21:58:55 -0300

    Adicionando controllers

--- WebUI/Controllers/WeatherForecastController.cs
+++ WebUI/Controllers/WeatherForecastController.cs
@@ -1,33 +1,15 @@
+using Application.WeatherForecasts.Queries.GetWeatherForecasts;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebUI.Controllers
+namespace WebUI.Controllers;
+
+
+public class WeatherForecastController : ApiControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get()
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+        return await Mediator.Send(new GetWeatherForecastsQuery());
     }
 }
